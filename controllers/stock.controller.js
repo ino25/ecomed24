fe92f97a4332c04d5f62666e455ceb32b06469a0
Neha maurya <nehasudha2021@gmail.com>
2025-05-27 17:04:55 +0530

    adjust stock

--- controllers/stock.controller.js
+++ controllers/stock.controller.js
@@ -235,10 +235,10 @@ exports.adjustStock = async (req, res) => {
       updated_by: req.userId
     });
 
-    res.status(200).json({ message: "Stock adjusted successfully", stock });
+    res.status(200).json({status:1, message: "Stock adjusted successfully", stock });
   } catch (error) {
     console.error("Stock adjustment error:", error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({status:0, message: "Internal server error" });
   }
 };
 
